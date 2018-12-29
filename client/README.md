a02647d7f84b5ca06c24110876292ce3351cd139
cbonoz <chrisdistrict@gmail.com>
2018-12-29 15:07:12 -0500

    first commit

---
+++ client/README.md
@@ -0,0 +1,5 @@
+<div align="center">
+    <img src="static/logos/logo-1024.png" alt="Logo" width='200px' height='200px'/>
+</div>
+
+# RepairX
---
+++ server/index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const lotion = require('lotion')
+const Hapi = require('hapi')
+
+// Create a server with a host and port
+const server = Hapi.server({
+    host:'localhost',
+    port:8001
+})
+
+// Add the route
+server.route({
+    method:'GET',
+    path:'/hello',
+    handler:function(request,h) {
+
+        return'hello world'
+    }
+})
+
+const app = lotion({
+  initialState: {
+    count: 0
+  }
+})
+
+app.use(function(state, tx) {
+    // State evolution handler
+  if (state.count === tx.nonce) {
+    state.count++
+  }
+})
+
+// Start the hapi server
+const start =  async function() {
+
+    // Start the tendermint server followed by the API.
+    try {
+        app.start()
+        await server.start()
+    }
+    catch (err) {
+        console.log(err)
+        process.exit(1)
+    }
+
+    console.log('Server running at:', server.info.uri)
+}
+
+start()
