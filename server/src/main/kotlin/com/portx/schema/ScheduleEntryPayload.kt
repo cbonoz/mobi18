f8d24345d1bbf8a288b1d6291dce1e27b0d10cdf
cbonoz <chrisdistrict@gmail.com>
2019-01-06 14:03:30 -0500

    update terminal filter

--- server/src/main/kotlin/com/portx/schema/ScheduleEntryPayload.kt
+++ server/src/main/kotlin/com/portx/schema/ScheduleEntryPayload.kt
@@ -7,7 +7,7 @@ data class ScheduleEntryPayload(
         val portId: String,
         val start: Long,
         val end: Long,
-        val terminal: String = "",
+        val terminal: String,
         val description: String = "",
         val owner: String = "PortX"
 )
