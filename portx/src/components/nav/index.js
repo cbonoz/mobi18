8b678d4e35fcfce23536761971c5df6c8c050e35
cbonoz <chrisdistrict@gmail.com>
2019-01-06 12:37:15 -0500

    add images and update header

--- portx/src/components/nav/index.js
+++ portx/src/components/nav/index.js
@@ -2,26 +2,29 @@ import React from 'react'
 import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap'
 import './style.css'
 
+import logo from './../../assets/portx_header.png'
+
 export default () => (
-  <Navbar>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#home">
-        <i class="fas fa-home"></i>
-        <span>PortX</span>
-      </a>
-    </Navbar.Brand>
-  </Navbar.Header>
-  <Nav pullRight>
-    <NavItem eventKey={1} href="/schedule">
-      Schedule A Pickup
+  <Navbar >
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#home">
+          {/* <i class="fas fa-home"></i> */}
+          <img src={logo} />
+          {/* <span>PortX</span> */}
+        </a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav pullRight>
+      <NavItem eventKey={1} href="/schedule">
+        Schedule A Pickup
     </NavItem>
-    <NavItem eventKey={2} href="/">
-      View Schedules
+      <NavItem eventKey={2} href="/">
+        View Schedules
     </NavItem>
-    <NavItem eventKey={3} href="/about">
-      About
+      <NavItem eventKey={3} href="/about">
+        About
     </NavItem>
-  </Nav>
-</Navbar>
+    </Nav>
+  </Navbar>
 )
