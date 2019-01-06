b4b3566d49c0b30c2f742c1100241a2c473b7d10
cbonoz <chrisdistrict@gmail.com>
2019-01-06 12:44:59 -0500

    add doc component

---
+++ portx/src/components/apidocs/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ApiDocs extends Component {
+    render() {
+        return (
+            <div>
+            <h1>API</h1>
+            </div>
+        );
+    }
+}
+
+export default ApiDocs;
--- portx/src/components/main/index.js
+++ portx/src/components/main/index.js
@@ -14,10 +14,10 @@ export default class MainApp extends React.Component {
   performSearch = (searchTerm) => {
     const ports = searchPorts(searchTerm)
     this.setState({ ports, focusedPort: ports[0] })
-      // .then(payload => {
-      //   const ports = payload.data
-      //   this.setState({ ports })
-      // })
+    // .then(payload => {
+    //   const ports = payload.data
+    //   this.setState({ ports })
+    // })
   }
 
   focusPort = port => {
@@ -27,18 +27,18 @@ export default class MainApp extends React.Component {
   }
 
   render() {
-    return <React.Fragment>
-    <Search
-        focusPort={this.focusPort} 
-        focusedPort={this.state.focusedPort} 
-        searchResults={this.state.ports} 
-        searchFn={this.performSearch}/>
+    return <div>
+      <Search
+        focusPort={this.focusPort}
+        focusedPort={this.state.focusedPort}
+        searchResults={this.state.ports}
+        searchFn={this.performSearch} />
 
-      <Map 
-        ports={this.state.ports} 
-        focusedPort={this.state.focusedPort} 
+      <Map
+        ports={this.state.ports}
+        focusedPort={this.state.focusedPort}
         focusPort={this.focusPort}
       />
-    </React.Fragment>
+    </div>
   }
 }
