ab1c8e97c70e9b7a9e70f31e7410c1c8c1f8307d
cbonoz <chrisdistrict@gmail.com>
2019-01-09 18:34:51 -0500

    update url

--- portx/src/helper/api.js
+++ portx/src/helper/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const PORT = 9005
 // const BASE_URL = `http://ec2-52-3-81-83.compute-1.amazonaws.com:${PORT}/api/portx`
-const BASE_URL = `https://www.goportx.com/api/portx`
+const BASE_URL = `goportx.com/api/portx`
 
 export function getPorts() {
     return axios.get(`${BASE_URL}/ports`)
